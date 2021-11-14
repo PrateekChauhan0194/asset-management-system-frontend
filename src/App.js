d4b31385c6b06c5f0e3e523381e7d8e4014c641e
Prateek <prateek.chauhan0194@gmail.com>
2021-11-14 14:26:59 +1100

    Toast alert and login component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 import Login from './components/Login/Login';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,50 @@
 import './Login.css';
 import React from 'react';
+import bcrypt from 'bcryptjs';
+import { toast } from 'react-toastify';
 
-const doLogin = (e) => {
+const doLogin = async (e) => {
     e.preventDefault();
-    console.log('doLogin');
+
+    const username = document.getElementsByName('username')[0].value;
+    const password = document.getElementsByName('password')[0].value;
+
+    const securePassword = await bcrypt.hash('A$$etSecPass', bcrypt.genSaltSync(10));
+    const isPasswordCorrect = await bcrypt.compare(password, securePassword);
+    if (isPasswordCorrect && username === 'admin') {
+        console.log('Login successful!');
+    } else {
+        toast.error('Incorrect username or password!', {
+            position: toast.POSITION.TOP_RIGHT,
+            hideProgressBar: true
+        });
+    }
 }
 
 const Login = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 mx-auto">
-                    <div className="card card-body">
-                        <h3 className="text-center mb-4">Asset management system - Login</h3>
-                        <form>
-                            <div className="form-group my-3">
-                                <label>Username</label>
-                                <input type="email" className="form-control" placeholder="Enter username" />
-                            </div>
-                            <div className="form-group my-3">
-                                <label>Password</label>
-                                <input type="password" className="form-control" placeholder="Enter password" />
-                            </div>
-                            <button type="submit" className="btn btn-primary btn-block" onClick={doLogin}>Login</button>
-                        </form>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <div className="card card-body">
+                            <h3 className="text-center mb-4">Asset management system - Login</h3>
+                            <form>
+                                <div className="form-group my-3">
+                                    <label>Username</label>
+                                    <input type="text" className="form-control" name="username" placeholder="Enter username" />
+                                </div>
+                                <div className="form-group my-3">
+                                    <label>Password</label>
+                                    <input type="password" name='password' className="form-control" placeholder="Enter password" />
+                                </div>
+                                <button type="submit" className="btn btn-primary btn-block" onClick={doLogin}>Login</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
