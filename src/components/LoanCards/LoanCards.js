def2827d2388685f6a46734c1f4754c83f5eddbc
Prateek <prateek.chauhan0194@gmail.com>
2021-11-19 20:42:55 +1100

    Added tooltips to the card action buttons

--- src/components/LoanCards/LoanCards.js
+++ src/components/LoanCards/LoanCards.js
@@ -4,7 +4,7 @@ import { isLoggedIn } from '../../services/AuthService';
 import Navbar from '../Navbar/Navbar';
 import { API_HOST } from '../../config';
 import AddLoanCard from '../AddLoanCard/AddLoanCard';
-import { Typography } from '@mui/material';
+import { Typography, Tooltip } from '@mui/material';
 import { Card } from 'react-bootstrap';
 
 const LoanCards = () => {
@@ -32,36 +32,6 @@ const LoanCards = () => {
                         <Typography variant="h2" className="title-loan-cards-list mt-5">
                             Loan cards
                         </Typography>
-                        {/* <table className="table table-striped mt-5">
-                            <thead>
-                                <tr>
-                                    <th>Service Number</th>
-                                    <th>Rank</th>
-                                    <th>Full Name</th>
-                                    <th>Department</th>
-                                    <th>Loaned Assets</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    borrowers.map((borrower, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <td>{borrower.serviceNumber}</td>
-                                                <td>{borrower.rank}</td>
-                                                <td>{borrower.fullName}</td>
-                                                <td>{borrower.department}</td>
-                                                <td>
-                                                    <Button variant="contained" size="small" color="primary" className="btn-loan-card">
-                                                        👁️ View
-                                                    </Button>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table> */}
 
                         <div className="container-loan-cards-list-cards">
                             {
@@ -73,9 +43,9 @@ const LoanCards = () => {
                                                 <Card.Subtitle className='mb-2 text-muted'>Service No: {borrower.serviceNumber}</Card.Subtitle>
                                                 <Card.Text>Section: {borrower.department}</Card.Text>
                                                 <div className='controls my-2 d-flex justify-content-end'>
-                                                    <i className="fas fa-eye  mx-1"></i>
-                                                    <i className="fas fa-edit mx-1"></i>
-                                                    <i className="fas fa-trash-alt delete-loan-card mx-1" />
+                                                    <Tooltip title="View"><i className="fas fa-eye mx-1"></i></Tooltip>
+                                                    <Tooltip title="Edit"><i className="fas fa-edit mx-1"></i></Tooltip>
+                                                    <Tooltip title="Delete"><i className="fas fa-trash-alt delete-loan-card mx-1" /></Tooltip>
                                                     {/* <i className='far fa-edit mx-2' data-bs-toggle='modal' data-bs-target='#edit-note-modal' /> */}
                                                 </div>
                                                 <Card.Footer><small><b>Created on:</b> {new Date(borrower.dataCreationDate).toDateString()}</small></Card.Footer>
