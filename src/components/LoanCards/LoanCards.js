aeb30ed6e48772eff2e60da58fa5070e9e86472f
Prateek <prateek.chauhan0194@gmail.com>
2021-11-20 00:26:56 +1100

    Delete loan card functionality

--- src/components/LoanCards/LoanCards.js
+++ src/components/LoanCards/LoanCards.js
@@ -6,6 +6,7 @@ import { API_HOST } from '../../config';
 import AddLoanCard from '../AddLoanCard/AddLoanCard';
 import { Typography, Tooltip } from '@mui/material';
 import { Card } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
 const LoanCards = () => {
 
@@ -20,6 +21,23 @@ const LoanCards = () => {
         const response = await fetch(`${API_HOST}/api/v1/borrower/getAll`);
         const data = await response.json();
         return data;
+    }
+
+    const deleteBorrower = async (id) => {
+        const response = await fetch(`${API_HOST}/api/v1/borrower/delete/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+        if (!data.errors) {
+            toast.success(data.msg);
+            setBorrowers(await getBorrowers());
+        } else {
+            toast.error(data.errors[0].msg);
+        }
     }
 
     return (
@@ -45,7 +63,7 @@ const LoanCards = () => {
                                                 <div className='controls my-2 d-flex justify-content-end'>
                                                     <Tooltip title="View"><i className="fas fa-eye mx-1"></i></Tooltip>
                                                     <Tooltip title="Edit"><i className="fas fa-edit mx-1"></i></Tooltip>
-                                                    <Tooltip title="Delete"><i className="fas fa-trash-alt delete-loan-card mx-1" /></Tooltip>
+                                                    <Tooltip title="Delete"><i className="fas fa-trash-alt delete-loan-card mx-1" onClick={() => deleteBorrower(borrower._id)} /></Tooltip>
                                                     {/* <i className='far fa-edit mx-2' data-bs-toggle='modal' data-bs-target='#edit-note-modal' /> */}
                                                 </div>
                                                 <Card.Footer><small><b>Created on:</b> {new Date(borrower.dataCreationDate).toDateString()}</small></Card.Footer>
