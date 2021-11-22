b2fb18e9e32b5daa086b09a7fef45cf8d7f37fc4
Prateek <prateek.chauhan0194@gmail.com>
2021-11-23 01:17:47 +1100

    feat: Delete item from the inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,6 +4,7 @@ import Navbar from '../Navbar/Navbar';
 import AddToInventory from '../AddToInventory/AddToInventory';
 import { getInventoryItems } from '../../services/APIComms';
 import { Typography } from '@mui/material';
+import { API_HOST } from '../../config';
 
 const Inventory = () => {
     const [inventoryItems, setInventoryItems] = useState([]);
@@ -12,6 +13,23 @@ const Inventory = () => {
         setInventoryItems(await getInventoryItems());
         // eslint-disable-next-line
     }, []);
+
+    const deleteItem = async (id) => {
+        // eslint-disable-next-line
+        const action = confirm('Are you sure you want to delete this item?');
+        if (action) {
+            const response = await fetch(`${API_HOST}/api/v1/item/deleteItem/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+            if (!data.errors) {
+                setInventoryItems(await getInventoryItems());
+            }
+        }
+    };
 
     return (
         isLoggedIn() ? (
@@ -32,17 +50,21 @@ const Inventory = () => {
                                     <th scope="col">Model</th>
                                     <th scope="col">Gig Number</th>
                                     <th scope="col">Data Creation Date</th>
+                                    <th scope="col">Delete</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {inventoryItems.map((inventoryItem, index) => (
-                                    <tr key={index}>
-                                        <td>{inventoryItem.name}</td>
-                                        <td>{inventoryItem.serialNumber}</td>
-                                        <td>{inventoryItem.model}</td>
-                                        <td>{inventoryItem.gigNumber}</td>
-                                        <td>{new Date(inventoryItem.dataCreationDate).toDateString()}</td>
-                                    </tr>
+                                    <>
+                                        <tr key={index}>
+                                            <td>{inventoryItem.name}</td>
+                                            <td>{inventoryItem.serialNumber}</td>
+                                            <td>{inventoryItem.model}</td>
+                                            <td>{inventoryItem.gigNumber}</td>
+                                            <td>{new Date(inventoryItem.dataCreationDate).toDateString()}</td>
+                                            <td><i className="fas fa-trash-alt delete-inventory-item mx-1" onClick={() => deleteItem(inventoryItem._id)} /></td>
+                                        </tr>
+                                    </>
                                 ))}
                             </tbody>
                         </table>
