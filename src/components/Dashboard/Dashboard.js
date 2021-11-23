c90c26fc2eef542341e09d4eb3e3fcc823275f74
Prateek <prateek.chauhan0194@gmail.com>
2021-11-24 01:54:27 +1100

    Placeholder dashboard text

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import { isLoggedIn } from '../../services/AuthService';
 import Navbar from '../Navbar/Navbar';
@@ -8,7 +9,31 @@ const Dashboard = () => {
             <>
                 <Navbar />
                 <div className="container">
-                    Dashboard works!
+                    <Typography variant="h2" gutterBottom className='mt-5'>Dashboard</Typography>
+                    <Typography variant="h3" gutterBottom className='mt-5'>Total Assets</Typography>
+                    Total owned assets: ___ <br />
+                    Item breakup based on name
+
+                    <br />
+                    <br />
+
+                    <Typography variant="h3" gutterBottom className='mt-5'>Assets in Inventory</Typography>
+                    Total assets in inventory: ___ <br />
+                    Item breakup based on name
+
+                    <br />
+                    <br />
+
+                    <Typography variant="h3" gutterBottom className='mt-5'>Loaned Assets</Typography>
+                    Total loaned assets: ___ <br />
+                    Item breakup based on name
+                    <br />
+                    <br />
+
+                    <Typography variant="h3" gutterBottom className='mt-5'>Defects</Typography>
+                    Should not issue item if the loan card with entered service number does not exist <br />
+                    Error while trying to edit the loan card with no change.
+                    <br />
                 </div>
             </>
         ) : (
