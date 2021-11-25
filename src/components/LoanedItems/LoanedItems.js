fbb6e4d58928d25207adce255e13152480c3e115
Prateek <prateek.chauhan0194@gmail.com>
2021-11-26 00:46:58 +1100

    feat: Dummy print icon

--- src/components/LoanedItems/LoanedItems.js
+++ src/components/LoanedItems/LoanedItems.js
@@ -51,7 +51,13 @@ const LoanedItems = ({ borrower, loanedItems, setLoanedItems, fetchLoanedItems }
                 <div className='modal-dialog modal-dialog-centered modal-xl'>
                     <div className='modal-content view-loaned-items'>
                         <div className='modal-header'>
-                            <h5 className='modal-title' id='edit-borrower-modal_title'><b>Loaned items</b> - {borrower.rank} {borrower.fullName} <span className='heading-service-number'>[{borrower.serviceNumber}]</span></h5>
+                            <h5
+                                className='modal-title'
+                                id='edit-borrower-modal_title'>
+                                <b>Loaned items</b> - {borrower.rank} {borrower.fullName} <span className='heading-service-number'>[{borrower.serviceNumber}] </span>
+                                <button className="btn btn-sm btn-outline-secondary" ><i className="fas fa-print" /> Print</button>
+                                {/* onClick={() => window.print()} */}
+                            </h5>
                             <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
                         </div>
                         <div className='modal-body'>
