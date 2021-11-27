3cfacd26a53e6759e21e1e9bf4588bbb30a04df2
Prateek <prateek.chauhan0194@gmail.com>
2021-11-27 15:10:42 +1100

    feat: Signup functionality

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,43 @@
 import './Signup.css';
 import React from 'react';
+import { toast } from 'react-toastify';
+import { API_HOST } from '../../config';
 
 const Signup = () => {
     const handleSignup = async (e) => {
         e.preventDefault();
+
+        const username = document.querySelector('div.signup-container input[name="signup-username"]').value;
+        const password = document.querySelector('div.signup-container input[name="signup-password"]').value;
+        const confirmPassword = document.querySelector('div.signup-container input[name="signup-confirm-password"]').value;
+
+        // Show error toast if passwrod and confirm password do not match
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match!');
+            return;
+        }
+
+        // Create API call to create user
+        const response = await fetch(`${API_HOST}/api/v1/auth/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            })
+        });
+
+        if (response.status === 200) {
+            toast.success('User created successfully!');
+            window.location.href = '/';
+        } else {
+            const data = await response.json();
+            toast.error(data.msg);
+        }
     }
+
     return (
         <>
             <div className="container signup-container">
@@ -22,8 +55,8 @@ const Signup = () => {
                                     <input type="password" name='signup-password' className="form-control" placeholder="Enter password" />
                                 </div>
                                 <div className="form-group my-3">
-                                    <label>Re-enter password</label>
-                                    <input type="password" name='signup-re-password' className="form-control" placeholder="Enter password" />
+                                    <label>Confirm password</label>
+                                    <input type="password" name='signup-confirm-password' className="form-control" placeholder="Enter password" />
                                 </div>
                                 <div className="d-flex justify-content-center">
                                     <button type="submit" className="btn btn-primary btn-block" onClick={handleSignup}>Signup</button>
