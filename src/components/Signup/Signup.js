145a78f7fa5420027f258964abd70b9efc7d24d0
Prateek <prateek.chauhan0194@gmail.com>
2021-11-27 14:26:28 +1100

    feat: Dummy signup form

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,10 +2,36 @@ import './Signup.css';
 import React from 'react';
 
 const Signup = () => {
+    const handleSignup = async (e) => {
+        e.preventDefault();
+    }
     return (
         <>
-            <div className="container">
-                <h1>Signup</h1>
+            <div className="container signup-container">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <div className="card card-body">
+                            <h3 className="text-center mb-4">Asset management system - Signup</h3>
+                            <form>
+                                <div className="form-group my-3">
+                                    <label>Username</label>
+                                    <input type="text" className="form-control" name="signup-username" placeholder="Enter username" />
+                                </div>
+                                <div className="form-group my-3">
+                                    <label>Password</label>
+                                    <input type="password" name='signup-password' className="form-control" placeholder="Enter password" />
+                                </div>
+                                <div className="form-group my-3">
+                                    <label>Re-enter password</label>
+                                    <input type="password" name='signup-re-password' className="form-control" placeholder="Enter password" />
+                                </div>
+                                <div className="d-flex justify-content-center">
+                                    <button type="submit" className="btn btn-primary btn-block" onClick={handleSignup}>Signup</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
