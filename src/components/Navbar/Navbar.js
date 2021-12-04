9d052a6979ebe28cd3a4a86e127713add0706522
Prateek <prateek.chauhan0194@gmail.com>
2021-12-04 16:45:14 +1100

    feat: Sticky navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
     const navigate = useNavigate();
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
