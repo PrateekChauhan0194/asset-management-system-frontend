18bb22ea46e3c68546ed62801906efe16ac772aa
Prateek <prateek.chauhan0194@gmail.com>
2021-12-09 23:17:36 +1100

    feat: Save item details in uppercase

--- src/components/AddToInventory/AddToInventory.js
+++ src/components/AddToInventory/AddToInventory.js
@@ -16,10 +16,10 @@ const AddToInventory = ({ setInventoryItems }) => {
             },
             body: JSON.stringify({
                 serviceNumber: 'inventory',
-                name: document.getElementById('form-add-to-inventory--text-item-name').value,
-                serialNumber: document.getElementById('form-add-to-inventory--text-item-serial-number').value,
-                model: document.getElementById('form-add-to-inventory--text-item-model').value,
-                gigNumber: document.getElementById('form-add-to-inventory--text-item-gig-number').value,
+                name: document.getElementById('form-add-to-inventory--text-item-name').value.toUpperCase(),
+                serialNumber: document.getElementById('form-add-to-inventory--text-item-serial-number').value.toUpperCase(),
+                model: document.getElementById('form-add-to-inventory--text-item-model').value.toUpperCase(),
+                gigNumber: document.getElementById('form-add-to-inventory--text-item-gig-number').value.toUpperCase(),
                 issueDate: new Date().toISOString(),
             }),
         });
