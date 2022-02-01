74b22894f8075794fb1ee81bacdfb06c173f6353
Prateek <prateek.chauhan0194@gmail.com>
2022-02-01 21:41:44 +1100

    feat: Print list of all borrowers

--- src/components/AllBorrowers/AllBorrowers.js
+++ src/components/AllBorrowers/AllBorrowers.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import PrintIcon from '@mui/icons-material/Print';
 import { Typography } from '@mui/material';
+import { parse } from 'node-html-parser';
 
 const AllBorrowers = ({ borrowers }) => {
     const hasBorrowers = borrowers.length > 0;
+
+    const handlePrint = async () => {
+        const date = new Date();
+        const strDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+        const printContents = parse(document.querySelector(`#view-borrowers .modal-body`).innerHTML);
+
+        const popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+        popupWin.document.open();
+        popupWin.document.write(`
+            <html>
+                <head>
+                    <title>Loan card holders</title>
+                    <style>
+                        @media print {
+                            @page {
+                                margin-top: 0mm;
+                            }
+                            .page-break {
+                                page-break-after: always;
+                            }
+                        }
+                        table, th, td {
+                            text-align: left;
+                            border: 1px solid black;
+                            border-collapse: collapse;
+                        }
+                        th, td {
+                            padding: 5px;
+                        }
+                    </style>
+                </head>
+                <body onload="window.print();window.close()">
+                        <hr/>
+                        <h2 style="text-align: center;">Loan card holders - ${strDate}</h2>
+                        <hr/>
+                        <h3>Number of borrowers: ${borrowers.length}</h3>
+                        <hr/>
+                        <div>${printContents}</div>
+                        <hr/>
+                </body>
+            </html>
+        `);
+        popupWin.document.close();
+    };
 
     return (
         <>
@@ -15,7 +61,7 @@ const AllBorrowers = ({ borrowers }) => {
                                 className='modal-title'
                                 id='view-borrowers-modal_title'>
                                 Loan Card holders
-                                <button className="btn btn-sm btn-outline-secondary m-2"><PrintIcon /> Print</button>
+                                <button className="btn btn-sm btn-outline-secondary m-2" onClick={handlePrint}><PrintIcon /> Print</button>
                             </h5>
                             <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
                         </div>
